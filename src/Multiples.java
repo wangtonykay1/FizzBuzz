96e1ee655591282d56ca455d5aa4005346d2465d
Tony Wang <tonywang@Tonys-MacBook-Pro.local>
2023-09-12 15:12:38 -0400

    for loop added

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,11 @@
 public class Multiples {
+    public static void main(String[] args) {
+        int total = 0;
+        for (int i = 0; i < 1000; i++){
+            if (i % 5 == 0 || i % 3 == 0){
+                total += 1;
+            }
+        }
+        System.out.println(total);
+    }
 }
