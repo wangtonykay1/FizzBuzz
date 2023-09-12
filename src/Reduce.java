04555f37bbbd8ddaeb277ed3ff8f13417c55016e
Tony Wang <tonywang@Tonys-MacBook-Pro.local>
2023-09-12 15:23:32 -0400

    prints number of steps taken

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,17 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+
+    public static void main(String[] args) {
+        int curr = 100;
+        int steps = 0;
+        while (curr != 0) {
+            if (curr % 2 == 0) {
+                curr /= 2;
+            }else{
+                curr -= 1;
+            }
+            steps++;
+        }
+        System.out.println(steps);
+    }
+
 }
